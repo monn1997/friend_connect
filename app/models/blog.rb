63023684b29f8d1b3b50941008fdefcb610f308b
tomomi adachi <tomoni1997@gmai.com>
2023-01-05 14:00:57 +0900

    バリデーションの追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,8 @@
 class Blog < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :title, presence: true
+  validates :image, presence: true
+  validates :content, presence: true,
+             length: { maximum: 255 }
   belongs_to :user
 end
